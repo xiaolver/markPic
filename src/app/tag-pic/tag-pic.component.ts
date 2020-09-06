f62f325ee0a05cfe3b813c91e365cd790e82fc63
xiaolvera <1165659662@qq.com>
2020-09-06 22:38:59 +0800

    提交忘了pull

--- src/app/tag-pic/tag-pic.component.ts
+++ src/app/tag-pic/tag-pic.component.ts
@@ -1,24 +1,18 @@
-import {
-  Component,
-  OnInit,
-  ElementRef,
-  ViewChild,
-  ViewChildren,
-  QueryList,
-} from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, ViewChildren, QueryList } from '@angular/core';
 import { RouterState } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import * as $ from 'jquery';
 
-import { GetScopeService } from '../tag-pic-service/get-scope.service';
+import {GetScopeService} from '../tag-pic-service/get-scope.service';
 import { getJSON } from 'jquery';
 import { stringify } from '@angular/compiler/src/util';
-import { DomSanitizer } from '@angular/platform-browser';
+import {DomSanitizer} from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-tag-pic',
   templateUrl: './tag-pic.component.html',
-  styleUrls: ['./tag-pic.component.css'],
+  styleUrls: ['./tag-pic.component.css']
 })
 export class TagPicComponent implements OnInit {
   @ViewChild('uploadInput', { static: true })
@@ -44,54 +38,51 @@ export class TagPicComponent implements OnInit {
   };
   ngOnInit(): void {
     // this.setfanwei();
+
   }
 
   constructor(
-    private getscope: GetScopeService,
-    private sanitizer: DomSanitizer
-  ) {
-    this.model = {
-      picurl:
-        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1599028409376&di=e4b60287b48c4f364045bbd99649e747&imgtype=0&src=http%3A%2F%2Fa1.att.hudong.com%2F05%2F00%2F01300000194285122188000535877.jpg',
-      crane: '',
-      lane: '',
-      video: '',
-      lorr: '',
-      degree: '',
-      xmin: '',
-      xmax: '',
-      ymin: '',
-      ymax: '',
-      imgurls: [],
-    };
-    console.log('come in constructor');
-    console.log(this.getscope);
-    console.log(this);
+    private getscope: GetScopeService, private sanitizer: DomSanitizer){
+      this.model = {
+        picurl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1599028409376&di=e4b60287b48c4f364045bbd99649e747&imgtype=0&src=http%3A%2F%2Fa1.att.hudong.com%2F05%2F00%2F01300000194285122188000535877.jpg',
+        crane: '',
+        lane: '',
+        video: '',
+        lorr: '',
+        degree: '',
+        xmin: '',
+        xmax: '',
+        ymin: '',
+        ymax: '',
+        imgurls: [],
+      };
+      console.log('come in constructor');
+      console.log(this.getscope);
+      console.log(this);
   }
-  getImgbyHttp(): void {
+  getImgbyHttp(): void
+  {
     console.log('getImg by http start');
-    this.model.imgurls = [
-      this.sanitizer.bypassSecurityTrustUrl(this.model.picurl),
-    ];
+    this.model.imgurls = [this.sanitizer.bypassSecurityTrustUrl(this.model.picurl)];
   }
-  getImgbyPath(): void {
+  getImgbyPath(): void
+  {
     console.log('getImg by path start');
     const inputFile = this.uploadInputRef.nativeElement.files;
     console.log('inputFile.length', inputFile.length);
-    this.model.imgurls = [].map.call(inputFile, (file) =>
-      this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(file))
-    );
+    this.model.imgurls = [].map.call(inputFile, file => this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(file)));
+
   }
-  clip(): void {
-    console.log('clip start ');
-    const picRefs = this.picsRef.toArray();
-    const piccanvasRefs = this.canvasshowpicsRef.toArray();
-    const canvasclipRefs = this.canvasclipsRef.toArray();
-    for (let i = 0; i < picRefs.length; i++) {
+   clip(): void{
+     console.log('clip start ');
+     const picRefs = this.picsRef.toArray();
+     const piccanvasRefs = this.canvasshowpicsRef.toArray();
+     const canvasclipRefs = this.canvasclipsRef.toArray();
+     for (let i = 0; i < picRefs.length; i++) {
       const image = picRefs[i].nativeElement;
 
-      const piccanvas = piccanvasRefs[i].nativeElement;
-      const canvas = canvasclipRefs[i].nativeElement;
+      const piccanvas =  piccanvasRefs[i].nativeElement;
+      const canvas =  canvasclipRefs[i].nativeElement;
       // tslint:disable: variable-name
       const x_min = Number(this.model.xmin);
       const x_max = Number(this.model.xmax);
@@ -110,27 +101,13 @@ export class TagPicComponent implements OnInit {
       console.log('imageWid imageHeight', image.width, image.height);
       piccanvas.width = image.width;
       piccanvas.height = image.height;
-      console.log(
-        'piccanvaswid piccanvasheight',
-        piccanvas.width,
-        piccanvas.height
-      );
+      console.log('piccanvaswid piccanvasheight', piccanvas.width, piccanvas.height);
 
-      ctxpic.drawImage(
-        image,
-        0,
-        0,
-        image.width,
-        image.height,
-        0,
-        0,
-        image.width,
-        image.height
-      );
+      ctxpic.drawImage(image, 0, 0, image.width, image.height, 0, 0, image.width, image.height);
       // }
-      // 画裁剪后的图片
-      // image.onload=function(){
-      const angleRad = ((((angle % 360) + 360) % 360) * Math.PI) / 180;
+    // 画裁剪后的图片
+    // image.onload=function(){
+      const angleRad = ((angle % 360 + 360) % 360) * Math.PI / 180;
       const sin = Math.sin(angleRad);
       const cos = Math.cos(angleRad);
       canvas.width = xcenter * Math.abs(cos) + ycenter * Math.abs(sin);
@@ -140,23 +117,13 @@ export class TagPicComponent implements OnInit {
         ctx.translate(ycenter * sin, 0);
       } else if (angleRad <= Math.PI) {
         ctx.translate(canvas.width, ycenter * -cos);
-      } else if (angleRad <= (Math.PI * 3) / 2) {
+      } else if (angleRad <= Math.PI * 3 / 2) {
         ctx.translate(xcenter * -sin, canvas.height);
       } else {
         ctx.translate(0, xcenter * cos);
       }
       ctx.rotate(angleRad);
-      ctx.drawImage(
-        piccanvas,
-        x_min,
-        y_min,
-        xcenter,
-        ycenter,
-        0,
-        0,
-        xcenter,
-        ycenter
-      );
+      ctx.drawImage(piccanvas, x_min, y_min, xcenter, ycenter, 0, 0, xcenter, ycenter);
       // 在标注图上画框，应该在画裁剪旋转图后，否则裁剪旋转图上会有边框
       ctxpic.rect(x_min, y_min, x_max - x_min, y_max - y_min);
       ctxpic.stroke();
@@ -164,31 +131,33 @@ export class TagPicComponent implements OnInit {
       // c
       //
 
-      // }
-      // var imgData=ctx.getImageData(Number(x_min),Number(y_min),Number(x_max),Number(y_max));
-      // ctx.putImageData(imgData,0,0);
+
+
+ // }
+    // var imgData=ctx.getImageData(Number(x_min),Number(y_min),Number(x_max),Number(y_max));
+    // ctx.putImageData(imgData,0,0);
     }
+
+
   }
-  setfanwei(): void {
+  setfanwei(): void
+  {
     console.log('setfanweiStart');
 
     console.log('crane,lane', this.model.crane, this.model.lane);
-    if (this.model.lorr === 'r') {
-      this.model.lane = String(Number(this.model.lane) + 10);
-    }
+    if ((this.model.lorr) === 'r'){this.model.lane = String(Number(this.model.lane) + 10); }
     console.log(this);
     console.log('getscope', this.getscope);
-    this.getscope
-      .getScope(this.model.crane, this.model.lane)
-      .subscribe(function (data): void {
+    this.getscope.getScope(this.model.crane, this.model.lane).subscribe(
+      function(data): void {
         this.datae = data;
         console.log('data', data);
-        console.log(typeof data);
+        console.log(typeof(data));
         const str = JSON.stringify(data);
-        console.log('str ', str, typeof str);
+        console.log('str ', str, typeof(str));
         let json = JSON.parse(str).info;
         const svideo = 'video' + this.model.video;
-        console.log('json', json[svideo]);
+        console.log('json', json[svideo] );
         json = json[svideo];
         console.log('xmin', json.x_min);
         console.log('xmax', json.x_max);
@@ -200,12 +169,18 @@ export class TagPicComponent implements OnInit {
         this.model.ymin = json.y_min;
         this.model.ymax = json.y_max;
         this.model.degree = json.degree;
-      });
+      }
+
+
+    );
+
+
+
+
 
     console.log('setfanweiEnd');
   }
-  downloadImage(): void {
-    // 下载图片地址和图片名
+  downloadImage(): void {// 下载图片地址和图片名
     const image = new Image();
     // 解决跨域 Canvas 污染问题
     image.setAttribute('crossOrigin', 'anonymous');
@@ -224,4 +199,5 @@ export class TagPicComponent implements OnInit {
     };
     image.src = this.model.picurl;
   }
+
 }
